4d7ccf70a123335f57487aa272b990c8a0b829d1
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-14 22:47:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :list
+  attr_accessor :parse
 
   def initialize(emails)
     @@list = emails.split(/,*\s*/)
